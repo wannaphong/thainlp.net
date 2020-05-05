e09ee66ba197c9117e06558a1bf8220f8a1b3e90
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2020-05-05 13:10:11 +0700

    change thainlp to Thainlp

--- thainlp/ThaiNLPTest/UnitTest1.cs
+++ thainlp/ThaiNLPTest/UnitTest1.cs
@@ -1,5 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using thainlp;
+using Thainlp;
 namespace ThaiNLPTest
 {
     [TestClass]
--- thainlp/thainlp/Program.cs
+++ thainlp/thainlp/Program.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
 
-namespace thainlp
+namespace Thainlp
 {
     class Program
     {
